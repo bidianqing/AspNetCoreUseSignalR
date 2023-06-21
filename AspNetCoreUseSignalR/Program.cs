b42ab931fa1c7c13062bbec7a074fcceb05c1be1
bidianqing <bidianqing@longfor.com>
2023-06-21 14:32:26 +0800

    up

--- AspNetCoreUseSignalR/Program.cs
+++ AspNetCoreUseSignalR/Program.cs
@@ -27,6 +27,6 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
-app.MapHub<NotificationHub>("/notificationhub");
+app.MapHub<NotificationHub>("/chathub");
 
 app.Run();
