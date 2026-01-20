462ba6f934098041a05479b582d660beaae09f16
bidianqing <bidianqing@qq.com>
2026-01-20 20:04:32 +0800

    *

--- AspNetCoreUseSignalR/NotificationHub.cs
+++ AspNetCoreUseSignalR/NotificationHub.cs
@@ -25,4 +25,11 @@ namespace AspNetCoreUseSignalR
             await base.Clients.Client(base.Context.ConnectionId).SendAsync("callClient", "hello", "world");
         }
     }
+
+    /* 使用Redis的Set数据结构存储无序不重复的数据集合
+    SADD	    添加元素（返回成功添加数）	SADD tags "java" "redis" 
+    SREM	    删除指定元素	            SREM tags "mysql"
+    SMEMBERS	获取所有元素	            SMEMBERS tags
+    SCARD	    返回元素数量	            SCARD tags
+    //*/
 }
