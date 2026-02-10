e80adf2beb3f2b4d41b3021f6e980d6ce95d8b93
bidianqing <bidianqing@qq.com>
2026-02-10 20:44:28 +0800

    *

--- AspNetCoreUseSignalR/NotificationHub.cs
+++ AspNetCoreUseSignalR/NotificationHub.cs
@@ -19,10 +19,9 @@ namespace AspNetCoreUseSignalR
         }
 
         // 客户端会Invoke这里的方法
-        public async Task SendMessage(string userId, string message)
+        public async Task SendMessage(string message)
         {
             await Console.Out.WriteLineAsync("收到客户端发来的消息:" + message);
-            await base.Clients.Client(base.Context.ConnectionId).SendAsync("callClient", "hello", "world");
         }
     }
 
